12b192e9ccfac14a9db9570fff6d5b719876c6b5
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-08-13 12:08:08 +0300

    added galery

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,24 +8,62 @@ export class Gallery extends Component {
     inputValue: '',
     currentPage: 1,
     photos: [],
-  }
+    buttonIsHidden: false,
+  };
 
-  onSubmit = (value) => {
+  onSubmit = value => {
     this.setState({
-      inputValue: value
-    })
-  }
+      inputValue: value,
+      currentPage: 1,
+      photos: [],
+    });
+  };
   searchImages = async () => {
-    const response = await ImageService.getImages('cars', 1)
-    console.log(response)
-    this.setState({ photos: response.photos })
+    const { inputValue, currentPage } = this.state;
+    try {
+      const { photos, per_page, total_results, page } =
+        await ImageService.getImages(inputValue, currentPage);
+      this.setState(prev => ({
+        photos: [...prev.photos, ...photos],
+        buttonIsHidden: page < Math.ceil(total_results / per_page),
+      }));
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      this.state.inputValue !== prevState.inputValue ||
+      this.state.currentPage !== prevState.currentPage
+    ) {
+      this.searchImages();
+    }
   }
 
+  onLoadMore = () => {
+    this.setState(prev => ({
+      currentPage: prev.currentPage + 1,
+    }));
+  };
   render() {
+    const { photos, buttonIsHidden } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.onSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        {photos.length > 0 ? (
+          <Grid>
+            {photos.map(({ id, avg_color, alt, src }) => (
+              <GridItem key={id}>
+                <CardItem color={avg_color}>
+                  <img src={src.large} alt={alt} />
+                </CardItem>
+              </GridItem>
+            ))}
+          </Grid>
+        ) : (
+          <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        )}
+        {buttonIsHidden && <Button onClick={this.onLoadMore}>Load more</Button>}
       </>
     );
   }
